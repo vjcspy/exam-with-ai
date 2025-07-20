551360e0d8d873febcba6409ed9d56bf914d0a93
win <mr.vjcspy@gmail.com>
2025-07-20 17:56:21 +0700

    Remove unused `jobs` API route and associated imports for cleanup.

--- src/app/api/jobs/route.ts
+++
@@ -1,8 +0,0 @@
-import { CommonValue } from '@/lib/values/common.value';
-
-export async function handle() {
-  const jobData = await fetch(CommonValue.getJobUrl());
-  return Response.json(await jobData.json());
-}
-
-export const GET = handle;
